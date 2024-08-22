c32aba2322eb75595f49c01f71572b005cf90b59
Dangwcuongw <dangcuongk87@gmail.com>
2024-08-22 20:24:35 +0700

    Add BTVN1

---
+++ BTVN1/index.php
@@ -0,0 +1,213 @@
+<!DOCTYPE HTML> 
+<html>
+<head>
+    <title>Starbuck Coffee Website</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet"href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+    <link rel="stylesheet" href="styless.css">
+</head>
+<body>
+    <header>
+        <div class="logo">
+            <a href="#"><img src="Image/logostb.png"></a>
+        </div>
+        <div class="navbar">
+            <ul >
+                <a href="#home">Home</a>
+                <a href="#aboutus">About Us</a>
+                <a href="#products">Products</a>
+                <a href="#Endless">Endless Extras</a>
+            </ul>
+        </div>
+        <div class="header-icons">
+            <i class="fa-solid fa-cart-shopping"></i>
+            <i class="fa-solid fa-magnifying-glass" id="icon-search" ></i>
+        </div>
+        <div class="search-box">
+            <input type="search" name="" id="" placeholder="Tìm kiếm ...">
+        </div>
+    </header>
+    <div id="home">
+            <img src="Image/Anhnenstb.jpg"/>
+    </div>
+    <div class="about" id="aboutus">
+        <div class="about-img">
+            <img src="Image/about-us.jpg"/>
+        </div>
+        <div class="about-text">
+            <h2>Our History</h2><br>
+            <p>Tất cả cá thức uống của Starbucks sẽ được phục vụ tận tay khách hàng ngay tại quầy bar của quán.
+                Đây là mối liên kết giữa Starbucks và khách hàng.</p>
+            <br>
+            <p>Stabucks cam kết mối liên hệ này xuất phát từ sự trân trọng của chúng tôi – chúng tôi cam kết phục vụ sản phẩm cà phê với chất lượng tốt nhất, tạo mối liên kết đến từng khách hàng và thực hiện trách nhiệm với cộng đồng và xã hội tại nơi Starbucks hoạt động kinh doanh.</p>
+            <br>
+            <p>Khởi nghiệp với một quán chuyên bán cà phê từ hơn 50 năm về trước, đến thời điểm hiện tại, tại mỗi một quốc gia mà Starbucks hướng tới, mỗi một địa điểm mà Starbucks dự định hoạt động kinh doanh, Starbucks cố gắng hoàn thiện và tạo dựng một môi trường tốt hơn những gì mà Starbucks đã làm.</p>
+        </div>
+    </div>
+    <div class="products" id="products">
+        <div class="con">
+            <h2>Our popular products</h2>
+        </div>
+        <div class="products-list">
+            <div class="box">
+                <img src="Image/Peppermint Mocha.webp">
+                <h3>Peppermint Mocha</h3>
+                <div class="products-gia">
+                    <span>4$</span>
+                    <a href="#">Add to cart</a>
+                </div>
+            </div>
+            <div class="box">
+                <img src="Image/Chocolate Chip Cream.png">
+                <h3>Chocolate Chip Cream</h3>
+                <div class="products-gia">
+                    <span>3.5$</span>
+                    <a href="#">Add to cart</a>
+                </div>
+            </div>
+            <div class="box">
+                <img src="Image/Ice green tea latte.png">
+                <h3>Ice green tea latte</h3>
+                <div class="products-gia">
+                    <span>5$</span>
+                    <a href="#">Add to cart</a>
+                </div>
+            </div>
+            <div class="box">
+                <img src="Image/Green tea Cream.png">
+                <h3>Green tea Cream</h3>
+                <div class="products-gia">
+                    <span>5$</span>
+                    <a href="#">Add to cart</a>
+                </div>
+            </div>
+            <div class="box">
+                <img src="Image/Espresso & Coffee.png">
+                <h3>Espresso & Coffee</h3>
+                <div class="products-gia">
+                    <span>8$</span>
+                    <a href="#">Add to cart</a>
+                </div>
+            </div> 
+            <div class="box">
+                <img src="Image/Cappuccino.png">
+                <h3>Cappuccino</h3>
+                <div class="products-gia">
+                    <span>10$</span>
+                    <a href="#">Add to cart</a>
+                </div>
+            </div>
+            <div class="box">
+                <img src="Image/Vanilla Latte.png">
+                <h3>Vanilla Latte</h3>
+                <div class="products-gia">
+                    <span>12$</span>
+                    <a href="#">Add to cart</a>
+                </div>
+            </div>
+            <div class="box">
+                <img src="Image/Caramel Macchiato.png">
+                <h3>Caramel Macchiato</h3>
+                <div class="products-gia">
+                    <span>15$</span>
+                    <a href="#">Add to cart</a>
+                </div>
+            </div> 
+            <div class="box">
+                <img src="Image/Starbucks® Caffè Latte.webp">
+                <h3>Starbucks® Caffè Latte</h3>
+                <div class="products-gia">
+                    <span>20$</span>
+                    <a href="#">Add to cart</a>
+                </div>
+            </div>
+            <div class="box">
+                <img src="Image/Caffè Mocha.webp">
+                <h3>Starbucks® Caffè Mocha</h3>
+                <div class="products-gia">
+                    <span>22$</span>
+                    <a href="#">Add to cart</a>
+                </div>
+            </div>
+            <div class="box">
+                <img src="Image/Starbucks® Caramel Latte.webp">
+                <h3>Starbucks® Caramel Latte</h3>
+                <div class="products-gia">
+                    <span>25$</span>
+                    <a href="#">Add to cart</a>
+                </div>
+            </div>   
+        </div>
+    </div>    
+    <div class="Endless-extra" id="Endless">
+        <div class="con">
+            <h2>Endless Extra</h2>
+        </div>
+        <div class="Extra-con">
+            <p>Joining Starbucks® Rewards means unlocking access to exclusive benefits. Say hello to easy ordering, tasty Rewards and—yes, free coffee.</p>
+        </div>
+        <div class="Extra-box">
+            <div class="box">
+                <img src="Image/endless-extra1.webp" alt="">
+                <h2>Fun freebies</h2>
+                <p>Not only can you earn free coffee, look forward to a birthday treat plus coffee and tea refills.</p>
+                <a href="#">Learn more</a>
+            </div>
+            <div class="box">
+                <img src="Image/endless-extra2.webp" alt="">
+                <h2>Order & pay ahead</h2>
+                <p>Enjoy the convenience of in-store, curbside or drive-thru pickup at select stores.</p>
+                <a href="#">Learn more</a>
+            </div>
+            <div class="box">
+                <img src="Image/endless-extra3.webp" alt="">
+                <h2>Get to free faster</h2>
+                <p>Earn Stars even quicker with Bonus Star challenges, Double Star Days and exciting games.</p>
+                <a href="#">Learn more</a>
+            </div>
+        </div>
+    </div>
+    <footer>
+        <div class="footer">
+            <div class="footer-box">
+                <h2>Starbucks® Coffee</h2>
+                <p>
+                    Inspiring and nurturing the human spirit – one person, one cup and one neighborhood at a time
+                </p>
+                <div class="icon-social">
+                    <a href="#"><i class="fa-brands fa-facebook"></i></a>
+                    <a href="#"><i class="fa-brands fa-twitter"></i></a>
+                    <a href="#"><i class="fa-brands fa-instagram"></i></a>
+                    <a href="#"><i class="fa-brands fa-tiktok"></i></a>
+                </div>
+            </div>
+            <div class="footer-box">
+                <h3>Support</h3>
+                <li><a href="#">Product</a></li>
+                <li><a href="#">Help & Support</a></li>
+                <li><a href="#">Return Policy</a></li>
+                <li><a href="#">Terms Of Use</a></li>
+            </div>
+            <div class="footer-box">
+                <h3>View Guides</h3>
+                <li><a href="#">Features</a></li>
+                <li><a href="#">Careers</a></li>
+                <li><a href="#">Blog Posts</a></li>
+                <li><a href="#">Our Branches</a></li>
+            </div>
+            <div class="footer-box">
+                <h3>Contacts</h3>
+                <div class="footer-contact">
+                    <span><i class="fa-solid fa-location-dot"></i>61 9th Ave, Thành phố New York, NY 10011-7001</span>
+                    <span><i class="fa-solid fa-phone"></i>+1 212-691-0531</span>
+                    <span><i class="fa-solid fa-envelope"></i>customerservice@coffee-concepts.com</span>
+                </div>
+            </div>
+        </div>
+        <div class="copyright">
+            <p>© 2024 Starbucks Coffee Company. All rights reserved.</p>
+        </div>
+    </footer>
+<script src="main.js"></script>
+</body>
+</html>
---
+++ BTVN1/main.js
@@ -0,0 +1,5 @@
+let search = document.querySelector('.search-box')
+
+document.querySelector('#icon-search').onclick = () => {
+        search.classList.toggle('active');
+}
